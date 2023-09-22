7911ebd987d8e544166e149f251f1920969b983b
franzSazon <136783797+franzSazon@users.noreply.github.com>
2023-09-22 10:51:27 +0800

    new notes

---
+++ Notes/amat_notes_22_09_2023.c
@@ -0,0 +1,42 @@
+/*
+    -AMAT NOTES 22/09/2023-
+
+    Statements, Expressions and Operators
+
+    #Relational Operators
+        - Used to compare expressions
+        - Ex. x == y; // is equal to?
+        - Ex. x != y; // is not equal to?
+        - Ex. x > y; // is greater than?
+        - Ex. x <= y; // is less than or equal to?
+        - Returns a boolean value Ex. 1 or 0, true or false
+        - All relational have lower precedence than other mathematical operators
+    
+    #Logical Operators
+        - Used to combine boolean expressions
+        - Ex. x && y; // logical and
+        - Ex. x || y; // logical or
+        - Ex. !x; // logical not
+        - Ex. ^x; // logical xor 
+        - Returns a boolean value Ex. 1 or 0, true or false
+
+    #Compound assignment operators
+        - Used to combine assignment operators
+        - Ex. x += y; // x = x + y
+        - Ex. x -= y; // x = x - y
+        - Ex. x *= y; // x = x * y
+        - Equal level of precedence, with left to right associativity
+        - Lowest precedence in terms of operators 
+
+    -> https://en.cppreference.com/w/c/language/operator_precedence //Operator Precedence List 
+
+
+    
+*/
+
+#include <stdio.h>
+ 
+int main(){
+    
+    return 0;
+}
