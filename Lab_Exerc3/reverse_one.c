a146edfd26b54b820bd2f4d1279905a438e7b420
franzSazon <136783797+franzSazon@users.noreply.github.com>
2023-09-27 20:50:16 +0800

    add better code comments

--- Lab_Exerc3/reverse_one.c
+++ Lab_Exerc3/reverse_one.c
@@ -2,17 +2,19 @@
  
 int main(){
 
+    // Declare Variables
     unsigned int num;
     int num1, num2;
 
+    // Collect User Input
     printf("Enter a two digit number: ");
     scanf("%d", &num);
 
-    //Print the values in reverse order
-
+    // Breakdown two digit number into singular digits
     num1 = num % 10;
     num2 = num / 10;
     
+    // Print the values in reverse order
     printf("%d", num1);
     printf("%d", num2);
     
